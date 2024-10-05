0ca0c0ad18541cdab62e00103b35bffc20e5fd41
Lawrence Lee <llee123@gmail.com>
2024-10-04 22:25:15 -0700

    Add shelly plus uni delegate

---
+++ src/device-delegates/shelly-plus-uni.ts
@@ -0,0 +1,23 @@
+import {
+  ShellyPlus1,
+  ShellyPlus1Ul,
+} from 'shellies-ng';
+
+import { DeviceDelegate } from './base';
+
+/**
+ * Handles Shelly Plus UNI devices.
+ */
+export class ShellyPlusUNIDelegate extends DeviceDelegate {
+  protected setup() {
+    const d = this.device as ShellyPlusUNI;
+
+    this.addSwitch(d.switch0, { single: true });
+    // this.addSwitch(d.switch1, { single: true });
+  }
+}
+
+DeviceDelegate.registerDelegate(
+  ShellyPlusUNIDelegate,
+  ShellyPlusUNI,
+);
